87c841c721d64ee63c68cd63f983015eeaf77f16
phpc0de <46225600+phpc0de@users.noreply.github.com>
2022-03-25 03:58:39 +0800

    Update nginx.sh

--- include/nginx.sh
+++ include/nginx.sh
@@ -20,7 +20,7 @@ Install_Nginx() {
 
   [ ! -d "${nginx_install_dir}" ] && mkdir -p ${nginx_install_dir}
   ./configure --prefix=${nginx_install_dir} --user=${run_user} --group=${run_group} --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-http_ssl_module --with-http_gzip_static_module --with-http_realip_module --with-http_flv_module --with-http_mp4_module --with-openssl=../openssl-${openssl11_ver} --with-pcre=../pcre-${pcre_ver} --with-pcre-jit --with-ld-opt='-ljemalloc' ${nginx_modules_options}
-  if [ -d "modsecurity-nginx-v${modsecurity_nginx_ver}" ]; then
+  if [ -d "modsecurity-nginx-v"${modsecurity_nginx_ver} ]; then
 
       make modules
       cp objs/ngx_http_modsecurity_module.so ${nginx_install}/modules 
