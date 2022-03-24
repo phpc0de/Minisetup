83d1a09404716b462bea2fb1b3ac21eb41548540
phpc0de <46225600+phpc0de@users.noreply.github.com>
2022-03-25 05:03:34 +0800

    Update modsecurity.sh

--- include/modsecurity.sh
+++ include/modsecurity.sh
@@ -5,14 +5,16 @@ Install_modsecurity() {
   else
     make clean all
     pushd ${oneinstack_dir}/src > /dev/null
-    tar xzf "modsecurity-"${modsecurity_ver}.tar.gz
-    tar xzf "modsecurity-nginx-v"${modsecurity_nginx_ver}.tar.gz
+    #tar xzf "modsecurity-"${modsecurity_ver}.tar.gz
+    #tar xzf "modsecurity-nginx-v"${modsecurity_nginx_ver}.tar.gz
     pushd "modsecurity-"${modsecurity_ver} > /dev/null
-    sh autogen.sh
+    git submodule init
+    git submodule update
+    sh ./build.sh
     ./configure --prefix=${modsecurity_install_dir} 
     make -j ${THREAD} && make install
     popd > /dev/null
-    rm -rf "modsecurity-"${modsecurity_ver}
+    #rm -rf "modsecurity-"${modsecurity_ver}
     popd > /dev/null
   fi
 }
