fadfc511315215f3ff884d7fc6f383ef667ca59b
phpc0de <46225600+phpc0de@users.noreply.github.com>
2022-03-24 19:04:01 +0800

    Update download.sh

--- include/download.sh
+++ include/download.sh
@@ -5,6 +5,6 @@ Download_src() {
   [ -s "${src_url##*/}" ] && echo "[${CMSG}${src_url##*/}${CEND}] found" || { wget --limit-rate=10M -4 --tries=6 -c  ${src_url}; sleep 1; }
   if [ ! -e "${src_url##*/}" ]; then
     echo "${CFAILURE}Auto download failed! You can manually download ${src_url} into the oneinstack/src directory.${CEND}"
-    kill -9 $$
+    kill -9 $$; exit 1;
   fi
 }
