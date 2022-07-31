1d2691257acea1b19c67e467ef77e13ab4ba19e6
phpc0de <46225600+phpc0de@users.noreply.github.com>
2022-08-01 04:10:34 +0800

    Update check_os.sh

--- include/check_os.sh
+++ include/check_os.sh
@@ -39,7 +39,7 @@ fi
 command -v gcc > /dev/null 2>&1 || $PM -y install gcc
 gcc_ver=$(gcc -dumpversion | awk -F. '{print $1}')
 
-[ ${gcc_ver} -lt 5 >/dev/null 2>&1 ] && redis_ver=${redis_oldver}
+[ ${gcc_ver} -lt 5 >/dev/null 2>&1 ] && sudo yum install centos-release-scl &&  sudo yum install devtoolset-9-gcc* && scl enable devtoolset-9 bash
 
 if uname -m | grep -Eqi "arm|aarch64"; then
   armplatform="y"
